bac02c559214cf9ddb28994fc8fae8d4a57c915f
Jorge <jorge1591@hotmail.com>
2022-07-01 20:50:09 -0500

    added text state, and useEffect to fetch data from an API to get images, updated the jsx and style to show the text properly when rendered

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,23 +1,21 @@
 import React from "react";
 import memeData from "../memesData"
 
- /**
-     * Challenge: Update our state to save the meme-related
-     * data as an object called `meme`. It should have the
-     * following 3 properties:
-     * topText, bottomText, randomImage.
+/**
+     * Challenge: 
+     * As soon as the Meme component loads the first time,
+     * make an API call to "https://api.imgflip.com/get_memes".
      * 
-     * The 2 text states can default to empty strings for now,
-     * amd randomImage should default to "http://i.imgflip.com/1bij.jpg"
+     * When the data comes in, save just the memes array part
+     * of that data to the `allMemes` state
      * 
-     * Next, create a new state variable called `allMemeImages`
-     * which will default to `memesData`, which we imported above
+     * Think about if there are any dependencies that, if they
+     * changed, you'd want to cause to re-run this function.
      * 
-     * Lastly, update the `getMemeImage` function and the markup 
-     * to reflect our newly reformed state object and array in the
-     * correct way.
+     * Hint: for now, don't try to use an async/await function.
+     * Instead, use `.then()` blocks to resolve the promises
+     * from using `fetch`. We'll learn why after this challenge.
      */
-    
 
 export default function Meme (){
 
@@ -27,13 +25,21 @@ export default function Meme (){
         randomImage: "http://i.imgflip.com/1bij.jpg"
     });
 
-    const [allMemeImages, setAllMemeImage] = React.useState(memeData);
-
+    const [allMemeImages, setAllMemeImage] = React.useState([])
+    
+    React.useEffect( () => {
+        console.log("call api")
+        fetch("https://api.imgflip.com/get_memes")
+            .then( res => res.json())
+            .then( resjson => setAllMemeImage(resjson.data.memes))
+        },
+        [meme.randomImage]
+    )
 
     function getRandMeme(){
-        const memesArray = allMemeImages.data.memes;
-        const randIndex = Math.floor(Math.random() * memesArray.length);
-        const url = memesArray[randIndex].url;
+        const memesArray = allMemeImages
+        const randIndex = Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randIndex].url
 
         setMeme( prevMeme => ({
             ...prevMeme, 
@@ -41,16 +47,44 @@ export default function Meme (){
         }))
     }
 
+    function handleText (e) {
+        const {name, value} = e.target
+        setMeme( prevMeme => (
+            {
+                ...prevMeme,
+                [name] : value
+            }
+        ))
+    }
+    console.log(meme)
+
     return (
         <div className="meme">
             <div className="meme--form">
-                <input className="meme--upper" type="text" name="caption-upper" placeholder="top text"/>
-                <input className="meme--lower" type="text" name="caption-lower" placeholder="bottom text"/>
+                <input 
+                    className="meme--upper" 
+                    type="text" 
+                    name="topText" 
+                    onChange={handleText} 
+                    placeholder="top text"
+                />
+                <input 
+                    className="meme--lower" 
+                    type="text" 
+                    name="bottomText" 
+                    onChange={handleText} 
+                    placeholder="bottom text"
+                />
 
                 <button onClick={getRandMeme} className="meme--submit" type="submit">Get a new meme image 🖼</button>
             </div>
-            <img className="meme--image" src={meme.randomImage} alt="Random meme generated"/>
+            <div className="meme">
+                <img className="meme--image" src={meme.randomImage} alt="Random meme generated"/>
+                <h1 className="meme--text top">{meme.topText}</h1>
+                <h1 className="meme--text bottom">{meme.bottomText}</h1>
+            </div>
+            
         </div>
         
-    );
+    )
 }
